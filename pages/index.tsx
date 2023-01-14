ede79aa4132ac8134d181fcb81d732eb1f4680c0
jenellehayes <118638171+jenellehayes@users.noreply.github.com>
2023-01-14 14:02:34 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Jenelle Hayes — Multidisciplinary Designer</title>
         <meta
           name="description"
-          content="Crafting well-thought visual identities and strategically designed website. Available for new projects in mid-March 2023."
+          content="Crafting well-thought visual identities and strategically designed website. Available for new projects mid-March 2023."
         />
       </Head>
       <main className="bg-primary text-white min-h-screen px-4 sm:px-10 py-10 grid place-content-center">
@@ -20,7 +20,7 @@ export default function Home() {
               <span>she/her</span>
             </div>
             <p>
-              Available to take on new projects in mid-March 2023. If you are
+              Available to take on new projects mid-March 2023. If you are
               interested in working together, please{" "}
               <a
                 href="mailto:hello@jenellehayes.com"
