df62759a1280c26ec5ff9a382888d64394c79804
jenellehayes <118638171+jenellehayes@users.noreply.github.com>
2023-01-21 09:18:37 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,9 +58,9 @@ export default function Home() {
               are all things I enjoy doing in my spare time. I care about the 
               environment and am trying to limit my impact on it. Formally 
               trained in new media production and design, as well as 
-              print communications. <a target=”_blank”
+              print communications. <a
                 href="https://drive.google.com/file/d/1sivzGO4B_5gr6uK23qG3UfiwEuuPLp6r/view"
-                className="font-medium italic"
+                className="font-medium italic" target=”_blank"
               ><u>
               Portfolio</u> coming soon!
               </a>
