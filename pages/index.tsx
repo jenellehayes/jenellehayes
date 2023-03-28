dec29243e5ee9a9c4c9ddf3fe8cada5ecd4da1b6
jenellehayes <118638171+jenellehayes@users.noreply.github.com>
2023-03-28 13:05:39 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,9 +32,7 @@ export default function Home() {
             </p>
             <div>
               <h2>Projects I Do</h2>
-              <p>
-                Create <strong>visual identities</strong> and <strong>brand standards</strong>, including <strong>logo design</strong>. <strong>Digital</strong> designs, including responsive <strong>websites</strong> made with <strong>accessibility</strong>, <strong>user experience</strong> and best practices in mind, such as <strong>SEO</strong> and <strong>information architecture</strong>. Fluent with all <strong>Adobe</strong> products, <strong>Figma</strong>, and I&apos;m currently learning <strong>Webflow</strong>. I&apos;ve developed <strong>social media strategies</strong>, template sets, mood boards, </strong>wireframes</strong>, and <strong>mockups</strong>. Print too! Experience with <strong>business cards</strong>, <strong>editorial</strong> layouts, <strong>adverts</strong>, <strong>signage</strong>, government and legal <strong>textbooks</strong>, and a <strong>cookbook</strong>.
-            </p>
+              <p>Create <strong>visual identities</strong> and <strong>brand standards</strong>, including <strong>logo design</strong>. <strong>Digital</strong> designs, including responsive <strong>websites</strong> made with <strong>accessibility</strong>, <strong>user experience</strong> and best practices in mind, such as <strong>SEO</strong> and <strong>information architecture</strong>. Fluent with all <strong>Adobe</strong> products, <strong>Figma</strong>, and I&apos;m currently learning <strong>Webflow</strong>. I&apos;ve developed <strong>social media strategies</strong>, template sets, mood boards, </strong>wireframes</strong>, and <strong>mockups</strong>. Print too! Experience with <strong>business cards</strong>, <strong>editorial</strong> layouts, <strong>adverts</strong>, <strong>signage</strong>, government and legal <strong>textbooks</strong>, and a <strong>cookbook</strong>.</p>
             </div>
           </div>
           <div>
@@ -44,11 +42,8 @@ export default function Home() {
               Counselling Centre, and the Government of Alberta.
             </p>
             <h2>About</h2>
-            <p>
-              Crafting in my homeland of <strong>Canada</strong> &#45; on traditional and ancestral territories of Treaty 7 and the Blackfoot Confederacy: Kainai, Piikani and Siksika, as well as the Tsuu T&apos;ina Nation and Stoney Nakoda First Nation, where I honour the histories, languages, and cultures of First Nations, Metis, Inuit, and all First Peoples of Canada &#45; I make with passion and care in Calgary, Alberta. Outside of honing my skills, I enjoy exploring nature, attending the orchestra, and visiting an art gallery or a museum. I care about the environment and limit my impact on it. I&apos;m formally trained in new media production and design, as well as print communications. 
-              <a href="https://drive.google.com/file/d/1sivzGO4B_5gr6uK23qG3UfiwEuuPLp6r/view" className="font-medium italic"><u> Portfolio</u> coming soon!
-              </a>
-            </p>
+            <p>Crafting in my homeland of <strong>Canada</strong> &#45; on traditional and ancestral territories of Treaty 7 and the Blackfoot Confederacy: Kainai, Piikani and Siksika, as well as the Tsuu T&apos;ina Nation and Stoney Nakoda First Nation, where I honour the histories, languages, and cultures of First Nations, Metis, Inuit, and all First Peoples of Canada &#45; I make with passion and care in Calgary, Alberta. Outside of honing my skills, I enjoy exploring nature, attending the orchestra, and visiting an art gallery or a museum. I care about the environment and limit my impact on it. I&apos;m formally trained in new media production and design, as well as print communications. 
+              <a href="https://drive.google.com/file/d/1sivzGO4B_5gr6uK23qG3UfiwEuuPLp6r/view" className="font-medium italic"><u> Portfolio</u> coming soon!</a></p>
           </div>
         </div>
       </main>
