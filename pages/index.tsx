a9c387fb718ec84bd33f2e9ae13de42227947de2
jenellehayes <118638171+jenellehayes@users.noreply.github.com>
2023-03-28 13:26:18 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,12 +36,12 @@ export default function Home() {
 standards</strong>. <strong>Digital</strong> designs, including responsive 
 <strong>websites</strong> made with <strong>accessibility</strong>, <strong>user 
 experience</strong> and best practices in mind, such as <strong>SEO</strong> and 
-<strong>information architecture</strong>. Fluent with all <strong>Adobe</strong> 
+<strong> information architecture</strong>. Fluent with all <strong>Adobe </strong> 
 products, <strong>Figma</strong>, and I&apos;m currently learning <strong>Webflow</strong>. 
 I&apos;ve developed <strong>social media strategies</strong>, template sets, mood boards, 
-<strong>wireframes</strong>, and <strong>mockups</strong>. Print too! Experience with 
-<strong>editorial</strong> layouts, <strong>adverts</strong>, <strong>signage</strong>, 
-<strong>business cards</strong>, government and legal <strong>textbooks</strong>, and 
+<strong> wireframes</strong>, and <strong>mockups</strong>. Print too! Experience with 
+<strong> editorial</strong> layouts, <strong>adverts</strong>, <strong>signage</strong>, 
+<strong> business cards</strong>, government and legal <strong>textbooks</strong>, and 
 a <strong>cookbook</strong>.</p>
             </div>
           </div>
